618750e5e08bbc5fe223b42f26b7b0295587b2f9
Kazehaya <elcatastroph91@gmail.com>
2025-12-04 11:42:25 +0700

    Delete PROJECT WEB SERVER directory

--- PROJECT WEB SERVER/Radhitya/script.js
+++
@@ -1,9 +0,0 @@
-// Animasi muncul setelah load
-document.addEventListener("DOMContentLoaded", () => {
-    const cards = document.querySelectorAll(".card");
-    cards.forEach((card, i) => {
-        setTimeout(() => {
-            card.style.opacity = "1";
-        }, i * 150);
-    });
-});
